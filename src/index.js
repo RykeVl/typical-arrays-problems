a1f04bafb7123474b3434009cc4037a528411272
RykeVl <iforox@gmail.com>
2020-10-03 17:15:55 +0300

    feat: added all features and fixed bugs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-
-exports.min = function min (array) {
-  return 0;
+function sorterino(a, b) {
+    return (a > b) ? 1 : -1;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    return array.sort(sorterino)[0];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    return array.sort(sorterino)[array.length - 1];
+}
+
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    return array.reduce((a, b) => { return a + b } ) / array.length;
 }
